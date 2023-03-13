0704a123e917a036e09bc837850e0cbf5ca7ba80
rilhan <rilhan1995@gmail.com>
2023-03-13 21:55:39 +0600

    added google scripts

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,10 +3,19 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>        
+      <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-        <link href="https://fonts.googleapis.com/css2?family=Gloock&family=Inter&display=swap" rel="stylesheet" />        
+        <link href="https://fonts.googleapis.com/css2?family=Gloock&family=Inter&display=swap" rel="stylesheet" />
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8190924517802964" crossorigin="anonymous"></script>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-V7CLJLF7W5"></script>
+        <script dangerouslySetInnerHTML={{
+              __html: `window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag(js'', new Date());
+          gtag('config', 'G-V7CLJLF7W5');
+          `, }}
+        />
       </Head>
       <body>
         <Main />
